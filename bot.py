b126ae526ef50eb9ceb4a57f1fd638b95a46b02d
mikey <mike.owino40@yahoo.com>
2022-07-03 18:09:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
+import os
 import requests
+my_secret = os.environ['API_KEY']
+my_secret1 = os.environ['CHAT_ID']
 
-api = "${{ secrets.API_KEY }}"
+api = my_secret
+chat_id = my_secret1
 
 def toTg():
 	image = "https://images.pexels.com/photos/1580625/pexels-photo-1580625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
-	url = "https://api.telegram.org/bot" + api + "/sendPhoto?chat_id=${{ secrets.CHAT_ID }}&photo=" + image
+	url = "https://api.telegram.org/bot" + api + "/sendPhoto?chat_id=" + chat_id + "&photo=" + image
+
 	payload={}
 	headers = {}
 	response = requests.request("POST", url, headers=headers, data=payload)
 	print(response.text)
-	dataa = response.text
 	
 for i in range(1):
 	toTg()
-
